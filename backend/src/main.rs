bb8fac5ff0370019cb128aeaab7d14f522de8596
Joshua Prieth <198204205+joshuaprieth@users.noreply.github.com>
2025-06-02 16:35:13 +0200

    Integrate the Wiktionary API

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,10 +1,17 @@
-use axum::{response::Html, routing::get, Router};
+use axum::extract::Path;
+use axum::http::StatusCode;
+use axum::response::Json;
+use axum::routing::get;
+use axum::Router;
+use reqwest::Client;
+use serde::{Deserialize, Serialize};
+use serde_json::{json, Value};
 use tokio::net::TcpListener;
 
 #[tokio::main(flavor = "current_thread")]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Build an axum router
-    let app = Router::new().route("/", get(handler));
+    let app = Router::new().route("/api/v1/spanish/word/{text}", get(look_up_spanish));
 
     // Create a tokio-based TCP listener
     let listener = TcpListener::bind("127.0.0.1:3000").await?;
@@ -17,6 +24,74 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 }
 
 // Handler for the root route
-async fn handler() -> Html<&'static str> {
-    Html("<h1>Hello, World!</h1>")
+async fn look_up_spanish(Path(text): Path<String>) -> Result<Json<Value>, StatusCode> {
+    let response = Client::new()
+        .get(format!(
+            "https://en.wiktionary.org/api/rest_v1/page/definition/{}",
+            text
+        ))
+        .send()
+        .await
+        .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
+
+    let status = response.status();
+
+    if status.is_success() {
+        let data: Value = response
+            .json()
+            .await
+            .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
+
+        let definitions = parse_wiktionary(data);
+
+        Ok(Json(json! {
+            {
+                "status": "ok",
+                "definitions": definitions
+            }
+        }))
+    } else if status.as_u16() == 404 {
+        // Still return a `200` status code here,
+        // so that the user can handle this at the
+        // response level instead of catching HTTP errors
+        Ok(Json(json! {
+            {
+                "status": "not found"
+            }
+        }))
+    } else {
+        Err(StatusCode::INTERNAL_SERVER_ERROR)
+    }
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+struct Dictionary {
+    part_of_speech: String,
+    definitions: Vec<Definition>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+struct Definition {
+    definition: String,
+    #[serde(default)]
+    examples: Vec<Example>,
+    #[serde(default)]
+    parsed_examples: Vec<ParsedExample>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Example(String);
+
+#[derive(Debug, Serialize, Deserialize)]
+struct ParsedExample {
+    example: String,
+    translation: String,
+}
+
+fn parse_wiktionary(mut data: Value) -> Option<Vec<Dictionary>> {
+    let spanish_section = data.get_mut("es")?.take();
+
+    serde_json::from_value(spanish_section).ok()
 }
